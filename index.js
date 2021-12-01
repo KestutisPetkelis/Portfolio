559b7387e75113245e2a7d0607249db5084212ed
unknown <k.petkelis@post.penki.lt>
2021-12-01 21:09:21 +0200

    Colors definitions and Light/Dark changes

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ const main = document.querySelector(".main")
 const content = document.querySelectorAll(".content")
 let indexContent 
 
+const button = document.querySelector("button")
+
+const greyColor = document.querySelectorAll(".greycolor")
+const colorWhite = document.querySelectorAll(".colorwhite")
+const colorBlack = document.querySelectorAll(".colorblack")
+
+const bColorWhite = document.querySelectorAll(".bcolorwhite")
+const bColorWht = document.querySelectorAll(".bcolorwht")
+const bColorEee = document.querySelectorAll(".bcoloreee")
+const bColorIcon = document.querySelectorAll(".bcoloricon")
+const bColorDarkDr = document.querySelectorAll(".bcolordarkgr")
+
+const boxShadow = document.querySelectorAll(".boxshadow")
+
+const borderTopGrey = document.querySelectorAll(".bordertopgrey")
+const borderColorEee = document.querySelectorAll(".bordercoloreee")
+const borderRcolorEee = document.querySelectorAll(".borderRcoloreee")
+
+let lightColor = true
+
 
 asideBtn.forEach((x, index) =>
 x.onclick =(e) =>{
@@ -27,4 +47,40 @@ x.onclick =(e) =>{
  
 }
 )
+button.onclick = () =>{
+    console.log("color")
+    if (lightColor){
+        greyColor.forEach(x => x.classList.add("d-greycolor"))
+        colorWhite.forEach(x => x.classList.add("d-colorwhite"))
+        colorBlack.forEach(x => x.classList.add("d-colorblack"))
 
+        bColorWhite.forEach(x => x.classList.add("d-bcolorwhite"))
+        bColorWht.forEach(x => x.classList.add("d-bcolorwht"))
+        bColorEee.forEach(x => x.classList.add("d-bcoloreee"))
+        bColorIcon.forEach(x => x.classList.add("d-bcoloricon"))
+        bColorDarkDr.forEach(x => x.classList.add("d-bcolordarkgr"))
+
+        boxShadow.forEach(x => x.classList.add("d-boxshadow"))
+
+        borderTopGrey.forEach(x => x.classList.add("d-bordertopgrey"))
+        borderColorEee.forEach(x => x.classList.add("d-bordercoloreee"))
+        borderRcolorEee.forEach(x => x.classList.add("d-borderRcoloreee"))
+    } else{
+        greyColor.forEach(x => x.classList.remove("d-greycolor"))
+        colorWhite.forEach(x => x.classList.remove("d-colorwhite"))
+        colorBlack.forEach(x => x.classList.remove("d-colorblack"))
+
+        bColorWhite.forEach(x => x.classList.remove("d-bcolorwhite"))
+        bColorWht.forEach(x => x.classList.remove("d-bcolorwht"))
+        bColorEee.forEach(x => x.classList.remove("d-bcoloreee"))
+        bColorIcon.forEach(x => x.classList.remove("d-bcoloricon"))
+        bColorDarkDr.forEach(x => x.classList.remove("d-bcolordarkgr"))
+
+        boxShadow.forEach(x => x.classList.remove("d-boxshadow"))
+
+        borderTopGrey.forEach(x => x.classList.remove("d-bordertopgrey"))
+        borderColorEee.forEach(x => x.classList.remove("d-bordercoloreee"))
+        borderRcolorEee.forEach(x => x.classList.remove("d-borderRcoloreee"))
+    }
+    lightColor = !lightColor
+}
