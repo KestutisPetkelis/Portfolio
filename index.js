2ea8c0fc7e552f8d1cd2448f88d52294014cca78
unknown <k.petkelis@post.penki.lt>
2021-12-01 15:18:13 +0200

    Add basic JS and change all content div's to d-flex

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 
 // 11-30 Portfolio
 
+const asideBtn = document.querySelectorAll(".asidediv")
+const main = document.querySelector(".main")
+const content = document.querySelectorAll(".content")
+let indexContent 
+
+
+asideBtn.forEach((x, index) =>
+x.onclick =(e) =>{
+    
+    for(i=0; i<asideBtn.length; i++) {
+        asideBtn[i].style.backgroundColor = "" // nusiimam mygtuko paspaudimo zymekli
+        
+    }
+        x.style.backgroundColor = "#FF014F" // uzdedam paspaudimo zymekli ant paspausto mygtuko
+        indexContent = index
+        content.forEach((x, index) =>{      // pasirenkam kuri kontenta rodyti pgl paspaudima
+            if (indexContent === index){
+                x.style.display = "flex"
+            }else{
+                x.style.display ="none"
+            }
+        })                                              
+    console.log("click")
+ 
+}
+)
 
